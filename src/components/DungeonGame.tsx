475f6f291fde38e80169d828b0af33129f833b81
fadi-gorges <fadi.gorges.03@gmail.com>
2023-08-17 22:24:57 +1000

    Added Dungeon of Death game

---
+++ src/components/DungeonGame.tsx
@@ -0,0 +1,76 @@
+'use client'
+import {Unity, useUnityContext} from "react-unity-webgl";
+import React, {useEffect, useState} from "react";
+import {ArrowsPointingOutIcon} from "@heroicons/react/24/outline";
+import {isMobile} from "react-device-detect"
+
+const DungeonGame = ({setModalOpen}: {
+    setModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
+    const {unityProvider, requestFullscreen, unload} = useUnityContext({
+        loaderUrl: "dungeon-build/Build/DungeonWeb.loader.js",
+        dataUrl: "dungeon-build/Build/DungeonWeb.data",
+        frameworkUrl: "dungeon-build/Build/DungeonWeb.framework.js",
+        codeUrl: "dungeon-build/Build/DungeonWeb.wasm",
+    })
+
+    const [devicePixelRatio, setDevicePixelRatio] = useState(window.devicePixelRatio);
+
+    useEffect(
+        function () {
+            const updateDevicePixelRatio = function () {
+                setDevicePixelRatio(window.devicePixelRatio);
+            };
+            const mediaMatcher = window.matchMedia(
+                `screen and (resolution: ${devicePixelRatio}dppx)`
+            );
+            mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+            return function () {
+                mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+            };
+        },
+        [devicePixelRatio]
+    );
+
+    const handleModalClose = () => {
+        unload()
+        setModalOpen(false)
+    }
+
+    return isMobile
+        ? <dialog id="dungeon-modal" className="modal modal-open">
+            <form method="dialog" className="modal-box">
+                <h3 className="font-bold text-lg">Dungeon of Death</h3>
+                <div className="py-4">
+                    Please play this game on a desktop computer.
+                </div>
+                <div className="modal-action">
+                    <button onClick={handleModalClose} className="btn">Close</button>
+                </div>
+            </form>
+            <form method="dialog" className="modal-backdrop">
+                <button onClick={handleModalClose}/>
+            </form>
+        </dialog>
+        : <dialog id="dungeon-modal" className="modal modal-open">
+            <form method="dialog" className="modal-box max-w-screen-xl">
+                <h3 className="font-bold text-lg">Dungeon of Death</h3>
+                <div className="py-4">
+                    <Unity unityProvider={unityProvider} devicePixelRatio={devicePixelRatio}
+                           className="w-full select-none"/>
+                </div>
+                <div className="modal-action">
+                    <button onClick={() => requestFullscreen(true)} className="btn btn-primary">
+                        Fullscreen
+                        <ArrowsPointingOutIcon className="w-6 h-6 ml-2"/>
+                    </button>
+                    <button onClick={handleModalClose} className="btn">Close</button>
+                </div>
+            </form>
+            <form method="dialog" className="modal-backdrop">
+                <button onClick={handleModalClose}/>
+            </form>
+        </dialog>
+}
+
+export default DungeonGame
